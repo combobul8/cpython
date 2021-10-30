e062b0af289162bfbd3d6180c8012da6d4fb2f1c
conbobul8 <you@example.com>
2021-10-30 12:46:23 +1100

    Results!

--- Objects/test_custom.py
+++ Objects/test_custom.py
@@ -11,27 +11,22 @@ with open(r"C:\Users\fooba\repos\cpython\Objects\words.txt") as f:
         word = line.strip()
         # print(word)
 
-        if i < 5000:
+        if i >= 5000:
+            break
+        elif word in words:
+            print("already seen " + word)
+        else:
             mycustom.update({word: i})
-
-            if (word in words):
-                print("already seen " + word)
-            else:
-                words.update({word: i})
+            words.update({word: i})
 
         i += 1
         # print("i: " + str(i))
 
-        if i >= 5000:
-            break
-            # mycustom.get(word)
-            None
-
 toc = time.perf_counter()
 # print(str((toc - tic) * 1000) + " milliseconds")
 
-print(len(mycustom.keys()))
+# print(len(mycustom.keys()))
 for key in list(mycustom.keys()):
     # print(mycustom.get(key))
-    # mycustom.get(key)
+    mycustom.get(key)
     None
