571e257acc2d5787103838461f63a58289c19c10
conbobul8 <you@example.com>
2022-01-06 23:09:16 +1100

    Meant key.

--- Objects/Custom.h
+++ Objects/Custom.h
@@ -2602,8 +2602,8 @@ custom_build_indices(CustomPyDictObject *mp, PyDictKeyEntry *ep, Py_ssize_t n)
                 mp->ma_indices_stack_idx--;
 
                 dictkeys_set_index(keys, hashpos, idx);
-                /* printf("build_indices set_index %lld %lld.\n", hashpos, idx);
-                fflush(stdout); */
+                printf("%s build_indices probe set_index %lld %lld.\n", PyUnicode_AsUTF8(ep->me_value), hashpos, idx);
+                fflush(stdout); /* */
 
                 mp->ma_indices_to_hashpos[idx] = hashpos;
 
@@ -2637,8 +2637,8 @@ custom_build_indices(CustomPyDictObject *mp, PyDictKeyEntry *ep, Py_ssize_t n)
                     mp->ma_indices_stack_idx--;
 
                     dictkeys_set_index(keys, hashpos0, idx);
-                    /* printf("build_indices set_index %lld %lld.\n", hashpos0, idx);
-                    fflush(stdout); */
+                    printf("build_indices post-filter set_index %lld %lld.\n", hashpos0, idx);
+                    fflush(stdout); /* */
 
                     mp->ma_indices_to_hashpos[idx] = hashpos0;
 
