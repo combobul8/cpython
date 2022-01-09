058aba05762cc87de5dba5333d445bd46d07c415
conbobul8 <you@example.com>
2022-01-09 22:24:26 +1100

    Revert "Some hashposs aren't appearing!"
    This reverts commit 7212d6365288ade21f285c27d3cbb4c7b1ff12b5.

--- Objects/Custom.h
+++ Objects/Custom.h
@@ -1826,9 +1826,6 @@ custom_build_indices(CustomPyDictObject *mp, PyDictKeyEntry *ep, Py_ssize_t n)
                     insertslot(mp, hashpos0, ep);
                 }
                 else {
-                    printf("%s insertlayer %lld.\n", PyUnicode_AsUTF8(ep->me_key), hashpos0);
-                    fflush(stdout);
-
                     insertlayer_keyhashvalue(layer, ep->me_key, ep->me_hash, ep->me_value);
                     mp->ma_num_items++;
                 }
